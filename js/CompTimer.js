53fa99f5a2473880803f549391c199b1add94dcf
tao-yu <taoxyu@gmail.com>
2023-07-15 20:06:51 +0100

    Write time formating functions

---
+++ js/CompTimer.js
@@ -0,0 +1,75 @@
+var timeStr = ""
+
+
+document.querySelectorAll('.keypad-button').forEach(item => {
+    item.addEventListener('click', event => {
+        // append the key number to the input field
+        let key = event.target.innerText;
+
+        if (key == "Submit" || key == "Backspace") {
+            console.log(key)
+        }
+        else {
+            if (timeStr.length < 6) {
+                timeStr += key;
+            }
+            let newTimeStr = formatUserTime(timeStr);
+            document.getElementById("time-entry").innerHTML = newTimeStr;
+            let centiseconds = getTimeCenti(newTimeStr)
+            console.log(centiseconds)
+            console.log(formatCenti(centiseconds))
+
+        }
+    })
+})
+
+function formatUserTime(oldTimeStr) {
+
+    let newTimeStr = oldTimeStr;
+    if (oldTimeStr.length == 1) {
+        return "0.0" + oldTimeStr;
+    }
+    if (oldTimeStr.length == 2) {
+        return "0." + oldTimeStr
+    }
+    if (oldTimeStr.length >= 3) {
+        newTimeStr = oldTimeStr.slice(0, oldTimeStr.length - 2) + "." + oldTimeStr.slice(oldTimeStr.length - 2);
+    }
+    if (oldTimeStr.length >= 5) {
+        newTimeStr = newTimeStr.slice(0, newTimeStr.length - 5) + ":" + newTimeStr.slice(newTimeStr.length - 5);
+    }
+
+    return newTimeStr;
+}
+
+function getTimeCenti(timeStr) {
+    /* Note that this function assumes a clean input */
+    if (timeStr.includes(":")) {
+        let parts = timeStr.split(":");
+        let minutes = parseFloat(parts[0]);
+        let seconds = parseFloat(parts[1]);
+        return Math.round(minutes * 6000 + seconds * 100);
+    }
+    else {
+        return Math.round(parseFloat(timeStr) * 100);
+    }
+}
+
+function formatCenti(centiseconds) {
+    let minutes = Math.floor(centiseconds / 6000);
+    let remainingCentiseconds = centiseconds % 6000;
+    let seconds = Math.floor(remainingCentiseconds / 100);
+    let finalCentiseconds = remainingCentiseconds % 100;
+
+    let timeString = '';
+    if (minutes > 0) {
+        timeString += minutes + ':';
+        // pad with a leading zero if seconds is less than 10
+        timeString += (seconds < 10 ? '0' : '') + seconds.toFixed(0);
+    } else {
+        timeString += seconds.toFixed(0);
+    }
+    // always show centiseconds, padded with a leading zero if necessary
+    timeString += '.' + (finalCentiseconds < 10 ? '0' : '') + finalCentiseconds.toFixed(0);
+    return timeString;
+}
